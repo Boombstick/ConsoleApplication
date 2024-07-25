84c2126b35314fae9d2879464b355d263414e60b
Boombstick <BoombStick.Devel@gmail.com>
2024-07-25 23:46:28 +0500

    Last Version

--- ConsoleApplication.UnitTests/AddEmployeeCommandTests.cs
+++ ConsoleApplication.UnitTests/AddEmployeeCommandTests.cs
@@ -5,7 +5,7 @@ using ConsoleApplication.Abstractions;
 
 namespace ConsoleApplication.UnitTests
 {
-    public class Tests
+    public class AddEmployeeCommandTests
     {
         private List<Employee> _employees;
         private Command _addCommand;
@@ -18,8 +18,6 @@ namespace ConsoleApplication.UnitTests
             {
                 new Employee(1,"John","Domond",100),
                 new Employee(2,"Artur","Morgan",100.100M),
-                new Employee(3,"Morgan","Freeman",10),
-                new Employee(4,"Arcadiy","Parovozov",0.12m)
             };
         }
 
@@ -37,15 +35,15 @@ namespace ConsoleApplication.UnitTests
         [TestCase( "FirstName:Maxim", "Brasov" )]
         [TestCase("Gena", "LastName:Bukin", "SalaryPerHour:101230,112312")]
         [TestCase("FirtName:Gena", "LastName:Bukin", "SalaryPerHour:101230,112312")]
-        [TestCase("FirtName:Gena", "LastName Bukin", "SalaryPerHour:101230,112312")]
-        [TestCase("FirtName:Gena", "LastName:Bukin", "SalaryPrHour:101230,112312")]
+        [TestCase("FirstName:Gena", "LastName Bukin", "SalaryPerHour:101230,112312")]
+        [TestCase("FirstName:Gena", "LastName:Bukin", "SalaryPrHour:101230,112312")]
         public void Should_ThrowException_When_ParameterNameIsInvalid(params string[] parameters)
         {
             Assert.Throws<ArgumentException>(() => _addCommand.Execute(_employees,parameters));
         }
 
         [TestCase("FirstName:Max12im", "Brasov")]
-        [TestCase("Gena", "LastName:Bukin", "SalaryPerHour:101230אפû112312")]
+        [TestCase("FirstName:Gena", "LastName:Bukin", "SalaryPerHour:101230אפû112312")]
         [TestCase("FirtName:Gena", "LastName:Bukin", "SalaryPerHour:101230sgas112312")]
         [TestCase("FirtName:Gena", "LastName Buk22in", "SalaryPerHour:101230,112312")]
         [TestCase("FirtName: ", "LastName:Bukin", "SalaryPrHour:101230,112312")]
